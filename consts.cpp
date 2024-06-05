048b3caabcc14e0bd104419ce3baccfe4f217f6a
Wiktor Leszczyński <wiktor.leszczynski@tlen.pl>
2024-06-05 21:10:05 +0200

    init

---
+++ consts.cpp
@@ -0,0 +1,9 @@
+#include "const.h"
+
+const char* ssid     = "ceska posta drti kocky";
+const char* password = "torbaborba8smake";
+const char* ntpServer = "0.pl.pool.ntp.org";
+const long  gmtOffset_sec = 0;
+const int   daylightOffset_sec = 7200;
+
+bool tak = false;
